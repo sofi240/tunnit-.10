6fbb076fb2406a2ec84409f1a6a042dedd18a174
sofi240 <sillaotssophia@gmail.com>
2024-10-03 10:58:13 +0300

    Add files via upload

--- tunnitoo.py
+++ tunnitoo.py
@@ -2,14 +2,14 @@
 
 # ül.1
 # konsooli printimine prindi konsooli tervitus
-
+# print("tere!")
 
 # ül.2
 # muuda oma kood nii et see küsiks kasutajalt nime ja tervitaks teda
 # vihje: sisend saab kutsuda funktsiooniga input()
 # kui on vaja esitada konkreetne küsimus
 # siis selle saab sõne andmetüübina kirjutada funtsiooni sisse
-
+# print(f"tere, {input("Mis on sinu nimi?")}!")
 # ül.2.1
 # muuda oma kood nii et kasutaja ei peaks mõtlema liigsetele tühikutele ja suurele algustähele
 
@@ -18,13 +18,13 @@
 # kontrolli oma teadmisi. Mida väljastavad kosooli kolm allolevat koodi
 # vastus kirjuta noole taha, seejärel kustuta rea alguses olev# ja kontorlli enast
 # print(type(7))  # ->
-# print(type("siin on mingisugune lause"))  # ->
+# print(type("siin on mingisugune lause"))  # ->string
 # print(type(7.0))  # ->
-
+# print(type("7.0"))
 
 # ül.3.2
 # kirjuta sulgudesse vastav pythoni termin
-# sõne (), täisarv(), ratsionaalarv()
+# sõne (str), täisarv(int), ratsionaalarv(float)
 
 # MUUTUJAD - kuidas hoida andmeid mälus
 # matemaatikast teame muutjuaid x ja y, kuid samuti teame et tegelikult võivad nad
@@ -37,3 +37,8 @@
 # Python 3.0 oli väljastatud aastal 2008
 release_year = 2008
 # mõtle välja loogiline nimetus muutujale, mis määrab pyhtoni vanust matemaatilise tehtena, pane see kirja
+minu_vanus_of_python = 16
+pythoni_vanus = 2024 - release_year
+C
+print(f"SUMMA:{a + b")
+
