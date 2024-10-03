6e49180883c9a217474f83a96cf142ac38261886
sofi240 <sillaotssophia@gmail.com>
2024-10-03 11:00:32 +0300

    Add files via upload

---
+++ praktiline_too.py
@@ -0,0 +1,11 @@
+"""Iseseisev praktiline töö"""
+
+# Kirjuta kood mis:
+# 1) küsib kasutajalt ainult täisarve a ja b
+# 2) tagastab kasutajale a ja b summa, korrutise ja ruutude summa
+# all on sulle vastuse näide koos andmetüübiga f-sõne, proovi jutumärkide sisse panna loogsulud {}
+a = int(input("sisesta oma a:"))
+b = int(input())
+print(f"SUMMA:{a + b}")
+print(f"KORRUTIS: {a * b}")
+print(f"RUUTUDE SUMMA:{a ** 2 + b ** ?}")
